ec4fda0740c237f0f305507f135f5fd6be074959
EVDOC88 <vovan8888@mail.ru>
2023-03-05 12:56:25 +0300

    Теория 11 модуля

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace Bot
+﻿using Telegram.Bot;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Bot.Controllers;
+using Bot.Services;
+using Bot.Configuration;
+
+namespace Bot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // Объект, отвечающий за постоянный жизненный цикл приложения
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services)) // Задаем конфигурацию
+                .UseConsoleLifetime() // Позволяет поддерживать приложение активным в консоли
+                .Build(); // Собираем
+
+            Console.WriteLine("Сервис запущен");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            AppSettings appSettings = BuildAppSettings();
+            services.AddSingleton(BuildAppSettings());
+
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Подключаем контроллеры сообщений и кнопок
+            services.AddTransient<DefaultMessageController>();
+            services.AddTransient<VoiceMessageController>();
+            services.AddTransient<TextMessageController>();
+            services.AddTransient<InlineKeyboardController>();
+
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(appSettings.BotToken));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+            services.AddSingleton<IFileHandler, AudioFileHandler>();
+
+        }
+        static AppSettings BuildAppSettings()
+        {
+            return new AppSettings()
+            {
+                DownloadsFolder = "E:\\TAUDIO",
+                BotToken = "6182354873:AAGWylqP0xbiPa7pDWwRiS4AhmBg4iBnO3E",
+                AudioFileName = "audio" + DateTime.Now.ToString("ddMMyyyyhhmmss"),
+                InputAudioFormat = "ogg",
+                OutputAudioFormat = "wav",
+                InputAudioBitrate = 44000
+
+            };
         }
     }
 }
---
+++ Bot/Services/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using Bot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Services
+{
+    public interface IStorage
+    {
+        /// <summary>
+        /// Получение сессии пользователя по идентификатору
+        /// </summary>
+        Session GetSession(long chatId);
+    }
+}
